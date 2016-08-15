0772734af130b796ffe71e73698ceb614228b1af
gs91ting <gs91ting@hotmail.com>
2016-08-15 20:23:27 +0900

    첫인사

---
+++ test/Big contest/Data.py
@@ -0,0 +1,7 @@
+class People:
+    def __init__(self,name1,name2):
+        self.name1=name1
+        self.name2=name2
+        print("%s님 %s님 빅콘테스트 참여를 진심으로 축하드립니다." %(self.name1,self.name2))
+
+Person1=People('장윤제','김동영')
