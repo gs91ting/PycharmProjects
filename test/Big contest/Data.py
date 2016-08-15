4cc04baea8aa3fbe4c250e30092a5520610738c4
gs91ting <gs91ting@hotmail.com>
2016-08-15 20:25:50 +0900

    인사말

--- test/Big contest/Data.py
+++ test/Big contest/Data.py
@@ -1,7 +1,9 @@
 class People:
+    Man_cnt=0
     def __init__(self,name1,name2):
         self.name1=name1
         self.name2=name2
         print("%s님 %s님 빅콘테스트 참여를 진심으로 축하드립니다." %(self.name1,self.name2))
-
+        Man_cnt+=2
+        print("현재 {}명의 참가자가 있습니다".format(Man_cnt))
 Person1=People('장윤제','김동영')
