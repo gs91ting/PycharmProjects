e3a6b8e6a5385e92be039b93b9193ccffb9b48c6
gs91ting <gs91ting@hotmail.com>
2016-08-16 21:34:55 +0900

    데이터 template 및 연령 그래프

--- test/Big contest/연령 그래프.py
+++ test/Big contest/연령 그래프.py
@@ -21,3 +21,90 @@ with open('사기 비사기 data.csv', 'r') as f:
         for i in row:
             a=i.split(',')
             matrix.append(a)
+data1=[] #사기자 age
+data2=[] #비사기자 age
+for i in range(1,len(matrix)):
+    if 'Y' in matrix[i][2]:
+        data1.append(int(matrix[i][4]))
+    if 'N'  in matrix[i][2]:
+        data2.append(int(matrix[i][4]))
+
+#사기자 나이 범위
+data1_0_10=[]
+data1_11_20=[]
+data1_21_30=[]
+data1_31_40=[]
+data1_41_50=[]
+data1_51_60=[]
+data1_61_70=[]
+data1_71_80=[]
+for i in data1:
+    if i<11:
+        data1_0_10.append(i)
+    if 10<i<21:
+        data1_11_20.append(i)
+    if 20<i<31:
+        data1_21_30.append(i)
+    if 30<i<41:
+        data1_31_40.append(i)
+    if 40<i<51:
+        data1_41_50.append(i)
+    if 50<i<61:
+        data1_51_60.append(i)
+    if 60<i<71:
+        data1_61_70.append(i)
+    if 70<i<81:
+        data1_71_80.append(i)
+
+
+
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+plotly.tools.set_credentials_file(username='gs91ting', api_key='87cmy6musf')
+
+
+
+data1_box=[go.Bar(
+                x=['0~10세','11~20세','21~30세','31~40세','41~50세','51~60세','61~70세','71~80세'],
+                y=[len(data1_0_10),len(data1_11_20),len(data1_21_30),len(data1_31_40),len(data1_41_50),len(data1_51_60),len(data1_61_70),len(data1_71_80)])]
+
+py.plot(data1_box, filename="사기자 연령")
+
+#비사기자 나이 범위
+data2_0_10=[]
+data2_11_20=[]
+data2_21_30=[]
+data2_31_40=[]
+data2_41_50=[]
+data2_51_60=[]
+data2_61_70=[]
+data2_71_80=[]
+data2_81_90=[]
+for i in data2:
+    if i<11:
+        data2_0_10.append(i)
+    if 10<i<21:
+        data2_11_20.append(i)
+    if 20<i<31:
+        data2_21_30.append(i)
+    if 30<i<41:
+        data2_31_40.append(i)
+    if 40<i<51:
+        data2_41_50.append(i)
+    if 50<i<61:
+        data2_51_60.append(i)
+    if 60<i<71:
+        data2_61_70.append(i)
+    if 70<i<81:
+        data2_71_80.append(i)
+    if 80<i<91:
+        data2_81_90.append(i)
+
+data2_box=[go.Bar(
+                x=['0~10세','11~20세','21~30세','31~40세','41~50세','51~60세','61~70세','71~80세','81~90세'],
+                y=[len(data2_0_10),len(data2_11_20),len(data2_21_30),len(data2_31_40),len(data2_41_50),len(data2_51_60),len(data2_61_70),len(data2_71_80),len(data2_81_90)])]
+
+py.plot(data2_box, filename="비사기자 연령")
+
+
